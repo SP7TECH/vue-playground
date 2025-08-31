5f4a01878ccff19805555f2d60af0b6d1db2dad4
SP7TECH <spandey247789@gmail.com>
2025-08-31 13:34:48 +0530

    favourite char component

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<script>
+import FavouriteCharacters from './components/FavouriteCharacters.vue'
+
+export default {
+  data() {
+    return {
+      movieName: 'Mission Impossible',
+      newCharacter: {
+        name: '',
+        age: 0,
+      },
+      characters: [
+        { name: 'Ethan Hunt', age: 59 },
+        { name: 'Luther', age: 52 },
+        { name: 'Benji', age: 49 },
+        { name: 'Julia', age: 42 },
+        { name: 'Ilsa', age: 40 },
+      ],
+      favourites: [],
+      // characters: [],
+    }
+  },
+  computed: {
+    calcAverageAge() {
+      const totalAge = this.characters.reduce((prev, curr) => prev + Number(curr.age), 0)
+      return totalAge / this.characters.length
+    },
+  },
+  methods: {
+    addCharacter() {
+      this.characters.push(this.newCharacter)
+      this.newCharacter = { name: '', age: 0 }
+    },
+    handleCharacterNameChange(e) {
+      this.newCharacter.name = e.target.value
+    },
+    handleCharacterAgeChange(e) {
+      this.newCharacter.age = e.target.value
+    },
+    markAsFavourite(name) {
+      this.favourites.push(name)
+    },
+  },
+  components: {
+    FavouriteCharacters,
+  },
+}
+</script>
+
+<template>
+  <h1>Exercises</h1>
+
+  <h2>{{ movieName }}</h2>
+
+  <h3>Add Characters</h3>
+
+  <div>
+    <label for="character">Character Name</label>
+    <input type="text" v-bind:value="newCharacter.name" v-on:input="handleCharacterNameChange" />
+    <input type="number" v-bind:value="newCharacter.age" v-on:input="handleCharacterAgeChange" />
+  </div>
+
+  <button v-on:click="addCharacter">Add Character</button>
+
+  <h3>Average Age of Characters: {{ calcAverageAge }}</h3>
+
+  <p>
+    <span v-for="(character, index) in characters"
+      >{{ character.name }}{{ index === characters.length - 1 ? '' : ', ' }}</span
+    >
+  </p>
+
+  <hr />
+
+  <FavouriteCharacters :favourites="favourites" />
+</template>
---
+++ src/components/FavouriteCharacters.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    favourites: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<template>
+  <h3>Favourite Characters</h3>
+
+  <ul v-if="favourites.length">
+    <li v-for="character in favourites">{{ character }}</li>
+  </ul>
+  <p v-else>No favourite characters found!</p>
+</template>
