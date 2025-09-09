606b3367fd1d76c9194ad3acd0df611f0c41bc2d
SP7TECH <spandey247789@gmail.com>
2025-09-09 08:07:01 +0530

    script setup

--- src/components/PokedexPage.vue
+++ src/components/PokedexPage.vue
@@ -1,19 +1,13 @@
-<script>
-export default {
-  data: () => ({
-    pokedex: [1, 2, 3],
-  }),
-  methods: {
-    async fetchPokemon() {
-      this.pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=5').then((response) =>
-        response.json(),
-      )
-    },
-  },
-  created() {
-    this.fetchPokemon()
-  },
+<script setup>
+import { ref } from 'vue'
+
+const pokedex = ref([])
+async function fetchPokemon() {
+  pokedex.value = await fetch('https://pokeapi.co/api/v2/pokemon?limit=5').then((response) =>
+    response.json(),
+  )
 }
+fetchPokemon()
 </script>
 
 <template>
